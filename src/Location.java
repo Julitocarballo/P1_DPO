63841bf1b546cead6323c35bc55017340cfc85a8
Julitocarballo <julio.carballo@students.salle.url.edu>
2018-11-30 19:55:04 +0100

    First Commit

---
+++ src/Location.java
@@ -0,0 +1,16 @@
+public class Location {
+    private float longitude;
+    private float latitude;
+
+    public Location() {
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.google.gson.stream.JsonReader;
+import com.google.gson.Gson;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public final class Main {
+
+    public static void main(String[] args) {
+
+        private static final String pokemons = "poke.json";
+
+        private static final String pokeballs = "balls.json";
+
+        private static final String llegendari = "legends.json";
+
+    }
+}
