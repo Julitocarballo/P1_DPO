1b0a30060a302be9adc4a3014e940ef236ce7d44
Julitocarballo <julio.carballo@students.salle.url.edu>
2018-12-03 16:37:10 +0100

    he acabat les classes, començat el main, menu i extreure dades

--- src/ExtreuDades.java
+++ src/ExtreuDades.java
@@ -1,4 +1,40 @@
 public class ExtreuDades {
     private Pokemons[] pokemons;
     private Pokeballs[] pokeballs;
+
+    public void execute(int opcio) {
+
+        switch (opcio) {
+
+            case 1:
+
+                //opcio1();
+                break;
+
+            case 2:
+
+                //opcio2();
+                break;
+
+            case 3:
+
+                //opcio3();
+                break;
+
+            case 4:
+
+                //opcio4();
+                break;
+
+            case 5:
+
+                //opcio5();
+                break;
+
+            case 6:
+
+                System.out.println("Fi programa.");
+                break;
+        }
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public final class Menu {
     private Scanner sc;
+    private String opcio;
+    private int opcion;
     public Menu() {
         sc = new Scanner(System.in);
+        opcio = "-";
+    }
+    public int getOpcio(){
+        return opcion;
+    }
+
+    public void setOpcion(int opcionn) {
+        opcion = opcionn;
     }
 
     public void mostraMenu() {
@@ -17,12 +27,64 @@ public final class Menu {
         System.out.println("7. Informe de Capturats ");
         System.out.println("8. Informació Detallada");
         System.out.println("9. Sortir\n\n");
-        System.out.println("Seleccioni una opcio:");
+
 
     }
 
     public boolean demanaOpcio(){
+        System.out.println("Seleccioni una opcio:");
+        opcio = sc.nextLine();
+        int error=0;
+        switch(opcio){
+            case "1":
+               opcion = 1;
+               setOpcion(opcion);
+               break;
+            case "2":
+                opcion = 2;
+                setOpcion(opcion);
+                break;
+            case "3":
+                opcion = 3;
+                setOpcion(opcion);
+                break;
+            case "4":
+                opcion = 4;
+                setOpcion(opcion);
+                break;
+            case "5":
+                opcion = 5;
+                setOpcion(opcion);
+                break;
+            case "6":
+                opcion = 6;
+                setOpcion(opcion);
+                break;
+            case "7":
+                opcion = 7;
+                setOpcion(opcion);
+                break;
+            case "8":
+                opcion = 8;
+                setOpcion(opcion);
+                break;
+            case "9":
+                opcion = 9;
+                setOpcion(opcion);
+                break;
+            default:
+                error=1;
+                break;
+        }
+        return (error!=0)?false:true;
 
+    }
+    public boolean continua () {
+
+        if (getOpcio() != 9) {
+            return true;
+        }
+        return false;
     }
 
 }
